2b61a0a03f8751dc03da5443cc5eb22bbd238cad
Pedro Diniz Rocha <pedro.diniz.rocha@gmail.com>
2025-08-01 10:06:57 -0300

    root

---
+++ cmd/root.go
@@ -0,0 +1,6 @@
+// Package cmd..
+package cmd
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/pedro/aurora
+
+go 1.24.5
