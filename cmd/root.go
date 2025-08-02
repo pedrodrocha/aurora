4b3fe126f3c0822bf471ef5b50d3ebe9826e8670
Pedro Diniz Rocha <pedro.diniz.rocha@gmail.com>
2025-08-02 18:00:03 -0300

    aurora root command

--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,23 @@
-// Package cmd..
+// Package cmd provides the entrypoint for aurora cli
 package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "aurora",
+	Short: "A system for bidirectional database schema translation",
+}
 
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
-func Execute() {}
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
--- main.go
+++ main.go
@@ -1,4 +1,7 @@
 package main
 
+import "github.com/pedro/aurora/cmd"
+
 func main() {
+	cmd.Execute()
 }
