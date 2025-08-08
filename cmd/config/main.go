09facb578044e7b5e8712bed446cc608d3045a13
Pedro Diniz Rocha <pedro.diniz.rocha@gmail.com>
2025-08-08 16:52:34 -0300

    execute form

--- cmd/config/main.go
+++ cmd/config/main.go
@@ -2,6 +2,7 @@
 package config
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/charmbracelet/huh"
@@ -15,53 +16,96 @@ func New() *cobra.Command {
 		Short: "Generate configuration file for Aurora",
 		Long:  "Generate interactively a config file for Aurora",
 		Run: func(cmd *cobra.Command, args []string) {
-			var (
-				selectedProvider provider.Provider
-				host             string
-				port             string
-				user             string
-				password         string
-			)
-
-			providerOptions := []huh.Option[provider.Provider]{}
-			for _, p := range provider.All() {
-				providerOptions = append(providerOptions, huh.NewOption(p.Label(), p))
-			}
-
-			form := huh.NewForm(
-				huh.NewGroup(
-					huh.NewSelect[provider.Provider]().
-						Title("Select a Provider").
-						Options(providerOptions...),
-
-					huh.NewInput().
-						Title("Host").
-						Value(&host),
-
-					huh.NewInput().
-						Title("Port").
-						Value(&port),
-					huh.NewInput().
-						Title("User").
-						Value(&user),
-					huh.NewInput().
-						Title("Password").
-						Value(&password),
-				),
-			)
-
-			if err := form.Run(); err != nil {
-				fmt.Println("Error running form:", err)
-				return
-			}
-
-			fmt.Println("Provider: ", selectedProvider)
-			fmt.Println("Host: ", host)
-			fmt.Println("Port: ", port)
-			fmt.Println("User: ", user)
-			fmt.Println("Password: ", password)
+			executeForm()
 		},
 	}
 
 	return configCommand
+}
+
+func executeForm() {
+	var (
+		selectedProvider provider.Provider
+		host             string
+		port             string
+		user             string
+		password         string
+		database         string
+		schema           = "public"
+	)
+
+	providerOptions := []huh.Option[provider.Provider]{}
+	for _, p := range provider.All() {
+		providerOptions = append(providerOptions, huh.NewOption(p.Label(), p))
+	}
+
+	form := huh.NewForm(
+		huh.NewGroup(
+			huh.NewSelect[provider.Provider]().
+				Title("Select a Provider").
+				Options(providerOptions...),
+
+			huh.NewInput().
+				Title("Database").
+				Value(&database).
+				Validate(func(str string) error {
+					if str == "" {
+						return errors.New("database is required")
+					}
+
+					return nil
+				}),
+
+			huh.NewInput().
+				Title("Schema").
+				Value(&schema),
+
+			huh.NewInput().
+				Title("Host").
+				Value(&host).
+				Validate(func(str string) error {
+					if str == "" {
+						return errors.New("host is required")
+					}
+
+					return nil
+				}),
+
+			huh.NewInput().
+				Title("Port").
+				Value(&port).
+				Validate(func(str string) error {
+					if str == "" {
+						return errors.New("port is required")
+					}
+					return nil
+				}),
+			huh.NewInput().
+				Title("User").
+				Value(&user).
+				Validate(func(str string) error {
+					if str == "" {
+						return errors.New("user is required")
+					}
+					return nil
+				}),
+
+			huh.NewInput().
+				Title("Password").
+				Value(&password),
+		),
+	).WithTheme(huh.ThemeBase16())
+
+	if err := form.Run(); err != nil {
+		fmt.Println("Error running form:", err)
+		return
+	}
+
+	fmt.Println("Provider: ", selectedProvider)
+	fmt.Println("Host: ", host)
+	fmt.Println("Port: ", port)
+	fmt.Println("User: ", user)
+	fmt.Println("Password: ", password)
+	fmt.Println("Database: ", database)
+	fmt.Println("Schema: ", schema)
 }
